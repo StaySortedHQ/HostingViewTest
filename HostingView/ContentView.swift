d5d044336e668a23c7ebb61813328574e5d25170
Joseph Cheung <josephktcheung@gmail.com>
2022-05-13 18:06:48 +0800

    Initial Commit

---
+++ HostingView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HostingView
+//
+//  Created by Joseph Cheung on 13/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HostingView/HostingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostingViewApp.swift
+//  HostingView
+//
+//  Created by Joseph Cheung on 13/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct HostingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
