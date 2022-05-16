cb8990d908ede6ddf3e317d300d7db2912449cc6
Joseph Cheung <josephktcheung@gmail.com>
2022-05-16 13:15:31 +0800

    Update readme

--- HostingView/UIHostingView.swift
+++ HostingView/UIHostingView.swift
@@ -1,9 +1,63 @@
 import SwiftUI
 import UIKit
 
+class HostingController<V: View>: UIHostingController<V> {
+    override func viewDidLoad() {
+        print("HostingController viewDidLoad")
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("HostingController viewWillAppear")
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("HostingController viewDidAppear")
+        // commenting below line out will not cause hosted view to re-render
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("HostingController viewWillDisappear")
+        // commenting below line out will not cause hosted view to re-render
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("HostingController viewDidDisappear")
+        super.viewDidDisappear(animated)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        print("HostingController viewWillTransition")
+        super.viewWillTransition(to: size, with: coordinator)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        print("HostingController viewDidLayoutSubviews")
+        super.viewDidLayoutSubviews()
+    }
+    
+    override func loadView() {
+        print("HostingController loadView")
+        super.loadView()
+    }
+    
+    override func willMove(toParent parent: UIViewController?) {
+        print("HostingController willMove", parent)
+        super.willMove(toParent: parent)
+    }
+    
+    override func didMove(toParent parent: UIViewController?) {
+        print("HostingController didMove", parent)
+        super.didMove(toParent: parent)
+    }
+}
+
 class UIHostingView<V: View>: UIView {
-    lazy var viewController: UIHostingController<V> = {
-        let vc = UIHostingController(rootView: view)
+    lazy var viewController: HostingController<V> = {
+        let vc = HostingController(rootView: view)
         vc.view.backgroundColor = .clear
         return vc
     }()
@@ -28,10 +82,10 @@ class UIHostingView<V: View>: UIView {
 
     override public func layoutSubviews() {
         super.layoutSubviews()
-        updatePageFrames()
+        updateFrames()
     }
 
-    private func updatePageFrames() {
+    private func updateFrames() {
         let screenHeight = UIScreen.main.bounds.height
         let frame = convert(bounds, to: nil)
         let y: CGFloat = frame.maxY > screenHeight ? 16 : 0
--- readme.md
+++ readme.md
@@ -7,3 +7,4 @@ This repo is to demonstrate when a parent UIViewRepresentable is dismissed, its 
 1. Run app, console should print `1 appear`
 2. Tap `To Normal`
 3. `1 appear` is printed on console, although we expect not to print anything as the parent view has been replaced by another view
+4. If we comment out `super.viewDidAppear` (line 18 of UIHostingView.swift) or `super.viewWillDisappear` (line 24 of UIHostingView.swift), the message `1 appear` will not be printed. However not calling `super` may cause other unwanted issues.
